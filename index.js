400500e753920a82371c15f4de2c75f5e38aba21
yeaeun23 <yeaeun23@naver.com>
2024-12-10 23:09:28 +0900

    기본 컨페티 클래스화

---
+++ index.js
@@ -0,0 +1,73 @@
+import Particle from "./js/Particle.js";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const dpr = window.devicePixelRatio > 1 ? 2 : 1;
+let canvasWidth, canvasHeight;
+const fps = 60;
+const interval = 1000 / fps;
+
+const particles = [];
+
+function init() {
+  canvasWidth = innerWidth;
+  canvasHeight = innerHeight;
+
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
+
+  canvas.width = canvasWidth * dpr;
+  canvas.height = canvasHeight * dpr;
+  ctx.scale(dpr, dpr);
+}
+
+function confetti({ x, y, count, deg }) {
+  for (let i = 0; i < count; i++) {
+    particles.push(new Particle(x, y, deg));
+  }
+}
+
+function render() {
+  let now, delta;
+  let then = Date.now();
+
+  const frame = () => {
+    requestAnimationFrame(frame);
+
+    now = Date.now();
+    delta = now - then;
+
+    if (delta < interval) {
+      return;
+    }
+
+    // 파티클 지우기
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    // 파티클 그리기
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update();
+      particles[i].draw(ctx);
+    }
+
+    then = now - (delta % interval);
+  };
+
+  requestAnimationFrame(frame);
+}
+
+window.addEventListener("click", () => {
+  confetti({
+    x: 0,
+    y: canvasHeight / 2,
+    count: 10,
+    deg: -50,
+  });
+});
+
+window.addEventListener("load", () => {
+  init();
+  render();
+});
+
+window.addEventListener("resize", init);
---
+++ js/Particle.js
@@ -0,0 +1,33 @@
+import { randomNumBetween } from "./utils.js";
+
+export default class Particle {
+  constructor(x, y, deg = 0) {
+    this.angle = (Math.PI / 180) * randomNumBetween(deg - 30, deg + 30);
+    this.r = randomNumBetween(30, 100);
+
+    this.x = x;
+    this.y = y;
+
+    this.vx = this.r * Math.cos(this.angle);
+    this.vy = this.r * Math.sin(this.angle);
+
+    this.gravity = 0.5;
+    this.friction = 0.89;
+
+    this.width = 30;
+    this.height = 30;
+  }
+  update() {
+    this.vy += this.gravity;
+
+    this.vx *= this.friction;
+    this.vy *= this.friction;
+
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
